3f5fb75dcfb5441f60fa71720c67ccd16dde4c24
amadou <amadou.balde.2023@student.lhschools.org>
2020-02-24 14:04:16 -0500

    Intellij commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("My name is");
+        System.out.println("Amadou");
+        //Print three more lines of what you did last summer
+        System.out.println("I played basketball");
+        System.out.println("I went to memphis to see my cousins");
+        System.out.println("I went to the movies");
+    }
+
+}
+
+
